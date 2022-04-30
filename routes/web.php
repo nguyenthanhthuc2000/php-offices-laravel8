8c05ac777cfa3d697880c5f8e0053910183a77da
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-04-30 19:48:00 +0700

    update login

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
+// Route::get('/', function () {
     //pass: 123456: $2y$10$dq5nQDcYVbJevCWkgHrqautMp.DQR9CsuRa247K66vavvvaBLj4HG
     // return csrf_token();
     // return view('welcome');
-});
+// });
 
-Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::match(['get', 'post'],'/login',[AuthController::class, 'index'])->name('login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+
 
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function(){
