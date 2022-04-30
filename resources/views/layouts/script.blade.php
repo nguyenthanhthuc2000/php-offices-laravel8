f0769ca127047fe6dffb54dea68342a568aa7ae8
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-04-30 22:15:01 +0700

    layout

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,3 @@
+
+    <script src="{{ asset('vendor/popper.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap.min.js') }}"></script>
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="vn">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@section('title', 'Đăng nhập')
+
+@section('content')
     @if ($errors)
         {{ $errors }}
     @endif
@@ -18,5 +12,4 @@
         <br>
         <button type="submit">login</button>
     </form>
-</body>
-</html>
+@endsection
