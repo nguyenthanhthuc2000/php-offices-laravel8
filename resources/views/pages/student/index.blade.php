67127014940cbf409f3965e1d4004bafb3dc6681
thanhnhan0974 <64397349+thanhnhan0974@users.noreply.github.com>
2022-05-01 18:26:39 +0700

    modify

--- resources/views/pages/student/index.blade.php
+++ resources/views/pages/student/index.blade.php
@@ -1,38 +1,43 @@
 @extends('layout')
 
+@section('title', 'Danh sách người dùng')
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">STT</th>
-            <th scope="col">Họ tên</th>
-            <th scope="col">Email</th>
-            <th scope="col">Mã số SV</th>
-            <th scope="col">Thao tác</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($students as $student)
-            <tr>
-                <th scope="row">1</th>
-                <td>{{$student->name}}</td>
-                <td>{{$student->email}}</td>
-                <td>{{$student->info->student_code}}</td>
-                <td>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                        <button class="btn btn-mute text-warning" type="button">
-                            <i class="fa-solid fa-user-pen"></i>
-                        </button>
-                        <button class="btn btn-mute text-danger" type="button">
-                            <i class="fa-solid fa-trash-can"></i>
-                        </button>
-                    </div>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    <div style="display: flex;justify-content: end;">
-        {{$students->links()}}
+    <div class="header-table mb-3 d-flex justify-content align-center">
+        <a class="btn btn-primary" href="{{ route('register') }}">
+            <i class="fa-solid fa-plus"></i> Thêm mới
+        </a>
+    </div>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Họ tên</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Chức vụ</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($lstUsers as $index => $user)
+                    <tr>
+                        <th scope="row">{{ ++$index }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ getNameRole($user->role) }}</td>
+                        <td>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button class="btn btn-mute text-warning" type="button">
+                                    <i class="fa-solid fa-user-pen"></i>
+                                </button>
+                                <button class="btn btn-mute text-danger" type="button">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </button>
+                              </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
