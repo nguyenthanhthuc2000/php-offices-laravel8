818e2759ba17459f4bd8d54d7f0eed23b7a974d4
Nguyễn Thành Thức <nguyenthanhthuc.2k@gmail.com>
2022-05-15 22:24:20 +0700

    slider

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,30 @@
 @section('content')
     <div>
         <h4 class="mb-3">Trang chủ</h4>
+
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('upload/slider/1.jpg') }}" style=" object-fit: contain;width: 100%;" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('upload/slider/2.jpg') }}" style=" object-fit: contain;width: 100%;" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('upload/slider/3.jpg') }}" style=" object-fit: contain;width: 100%;" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <br>
         <div class="row">
             @foreach($news as $n)
                 <div class="col-md-3 mb-3">
--- routes/web.php
+++ routes/web.php
@@ -48,12 +48,14 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/tin-tuc/update/{id}', [NewsController::class, 'update'])->name('news.update');
         Route::get('/cap-nhat-tin-tuc/{id}', [NewsController::class, 'edit'])->name('news.edit');
         Route::post('/xoa-bai-viet/{id}', [NewsController::class, 'delete'])->name('news.delete');
-    });
+        Route::get('/chinh-sua-thong-tin-sinh-vien/{id}', [StudentController::class, 'edit'])->name('student.edit');
 
+    });
     Route::get('/xoa-sinh-vien/{id}', [UserController::class, 'delete'])->name('student.delete')->middleware('isAdmin', 'isTeacher');
+
+
     Route::get('/danh-sach-sinh-vien', [StudentController::class, 'index'])->name('student.index');
     Route::get('/thong-tin-sinh-vien/{id}', [StudentController::class, 'detail'])->name('student.detail');
-    Route::get('/chinh-sua-thong-tin-sinh-vien/{id}', [StudentController::class, 'edit'])->name('student.edit');
     Route::get('/profile', [StudentController::class, 'profile'])->name('profile');
     Route::get('/xuat-bieu-mau', [FileController::class, 'export'])->name('file.dowload');
     Route::get('/tin-tuc/{slug}', [NewsController::class, 'detail'])->name('news.detail');
